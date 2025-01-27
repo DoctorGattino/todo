77b5fad31b1d6fa7e0441bce15151640a116facc
DoctorGattino <Doctorgattino@gmail.com>
2025-01-27 21:46:18 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('Hello world!');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
