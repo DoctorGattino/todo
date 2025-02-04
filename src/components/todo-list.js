804f0c7bd7a5e98cbb3cfbdfb2a9c3bbe67e58f7
DoctorGattino <Doctorgattino@gmail.com>
2025-02-04 21:51:59 +0300

    123

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,17 +2,22 @@ import React from "react";
 
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
-  return (
-    <ul>
-      <li>
-        <TodoListItem label="Drink Coffee" />
+/*       <li>
+<TodoListItem label={item.label} important={item.important} />
+</li> */
+
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id}>
+        <TodoListItem {...itemProps} />
       </li>
-      <li>
-        <TodoListItem label="Build React App" important />
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul>{elements}</ul>;
 };
 
 export default TodoList;
